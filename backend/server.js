5671eafaf486af4dc6ab467572b4fef41720debf
48-JASHWANTH <ratnajashwanth64@gmail.com>
2024-05-19 15:39:32 +0530

    Successful connection of server and sqlServer(database)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const app = express()
+const app = express();
 require("dotenv").config();
+const sqlServer = require("mssql");
 
+const dbConfig = {
+  user: process.env.user,
+  password: process.env.password,
+  server: process.env.server,
+  database: process.env.database,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+};
+
+async function connectToDatabase() {
+  try {
+    await sqlServer.connect(dbConfig);
+    console.log("Connected to the SQL Server database");
+  } catch (err) {
+    console.error("Database connection failed:", err);
+  }
+}
+
+connectToDatabase();
 
 //express error handler
-app.use((error,request,response,next)=>{
-    response.send({message:'error',payload:error.message})
-})
+app.use((error, request, response, next) => {
+  response.send({ message: "error", payload: error.message });
+});
 
 //Assign port number
 const port = process.env.PORT || 7777;
